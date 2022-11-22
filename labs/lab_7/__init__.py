3120212de6b316f9e268b94e214b7bc7c2d0fc42
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-11-22 13:57:30 +0200

    I think i made progress so ill save this state

--- labs/lab_7/__init__.py
+++ labs/lab_7/__init__.py
@@ -4,5 +4,6 @@ from labs.lab_7.decision_tree import build_decision_tree
 
 
 def run():
-    df = pd.read_csv('labs/content/drug200.csv')
-    tree = build_decision_tree(df, 'Drug')
+    df = pd.read_csv('labs/content/tennis.csv')
+    tree = build_decision_tree(df, 'Play')
+    print(tree)
--- labs/lab_7/decision_tree/__init__.py
+++ labs/lab_7/decision_tree/__init__.py
@@ -2,28 +2,38 @@ import numpy as np
 import pandas as pd
 
 
-def e(val):
+def eqn(val):
     return val * np.log10(val)
 
 
 def e_feat(feat_val_grp: pd.DataFrame, label: str):
     total = len(feat_val_grp)
-    return - sum([e(len(i)/total) for n, i in feat_val_grp.groupby(label)])
+    return - sum([eqn(len(i) / total) for n, i in feat_val_grp.groupby(label)])
 
 
-def get_entropy(feat_grp, label, total_count):
-    return sum([(len(val)/total_count) * e_feat(val, label) for n, val in feat_grp])
+def e(feat_grp, label, total_count):
+    return sum([(len(val) / total_count) * e_feat(val, label) for n, val in feat_grp])
 
 
-def tree_util(df: pd.DataFrame, tree):
+def tree_util(df: pd.DataFrame, label, tree):
+    for i, val in df.groupby(label):
+        tree = tree_util(val, label, tree)
     return tree
 
 
-def build_decision_tree(df: pd.DataFrame, lbl):
-    entropy_values = dict(sorted({f: get_entropy(df.groupby(f), lbl, len(df)) for f in df}.items(), key=lambda i: i[1]))
-    del entropy_values[lbl]
-    tree = {}
+def build_decision_tree(df: pd.DataFrame, label, visited=None, tree: dict = None):
+    if visited is None:
+        visited = []
+    entropy_values = dict(sorted({f: e(df.groupby(f), label, len(df)) for f in df}.items(), key=lambda x: x[1]))
+    del entropy_values[label]
+    if tree is None:
+        tree = {f: {} for f in entropy_values}
+    if len(df[label].unique()) == 1:
+        return df[label].unique()[0]
     for feature, entropy in entropy_values.items():
-        for i, val in df.groupby(feature):
-            tree = tree_util(val, tree)
-
+        for i, feat_val in df.groupby(feature):
+            if i in visited:
+                continue
+            visited.append(i)
+            tree[feature].update({i: build_decision_tree(feat_val, label, visited, tree)})
+    return tree
