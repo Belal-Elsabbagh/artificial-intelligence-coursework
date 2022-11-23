9f27e4966969f12f9da6eb04c03e2f68a6254a2f
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-11-23 08:57:06 +0200

    Cleaned code into modules

--- labs/lab_7/__init__.py
+++ labs/lab_7/__init__.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-from labs.lab_7.decision_tree import build_decision_tree
+from labs.lab_7.id3 import id3
 
 
 def run():
     df = pd.read_csv('labs/content/tennis.csv')
-    tree = build_decision_tree(df, 'Play')
+    tree = id3(df, 'Play')
     print(tree)
